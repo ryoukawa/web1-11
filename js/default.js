a2c10dcb0bb29f095ba5861280eaced455c38d0b
ryoukawa <ryou1110.pkmn@gmail.com>
2017-06-19 17:16:33 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,3 +1,16 @@
-window.alert('こんにちは！');
-console.log('こんにちは！');
+
+var namber = Math.floor(Math.random()*10);
+var answer = parseInt(window.prompt('数あてゲーム。0～10の数字を入力してください！'));
+
+var message;
+if(answer === number){
+  message =　'当たり';
+}else if (answer < number){
+  message = '残念！もっと大きい数字です';
+}else if (number < answer){
+  message = '残念！もっと小さい数字です';
+}else{
+  message = '0～10 の数字を入力してください';
+}
+                        
 
